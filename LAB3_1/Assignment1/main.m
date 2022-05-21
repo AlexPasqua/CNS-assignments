34fb45ad49157b54b53075df18d754e8a73320c2
AlexPasqua <alexpasquali98@gmail.com>
2022-05-21 22:49:30 +0200

    LAB3_1: minor change

--- LAB3_1/Assignment1/main.m
+++ LAB3_1/Assignment1/main.m
@@ -18,13 +18,13 @@ n_trials = 10;
 max_inputDelay = 10;
 max_hiddenSize = 50;
 max_n_hiddenLayers = 5;
-train_funcs = {'traingd', 'traingdm', 'traingdx', 'trainrp', 'trainlm'};
+train_funcs = ["traingd", "traingdm", "traingdx", "trainrp", "trainlm"];
 for i = 1 : n_trials
     % randomly select hyperparameters
     inputDelays = randi(max_inputDelay);
     hiddenSizes = randi(max_hiddenSize, [1 randi(max_n_hiddenLayers)]);
     lr = rand;
-    trainFcn = cell2mat(datasample(train_funcs, 1));
+    trainFcn = train_funcs(randi(length(train_funcs)));
     conf = struct('inputDelays', inputDelays, 'hiddenSizes', hiddenSizes, ...
         'lr', lr, 'trainFcn', trainFcn);
     
@@ -42,23 +42,23 @@ for i = 1 : n_trials
     
     % prepare the time series for the two nets
     % TDNN training set
-    [tdnn_Xs_tr, tdnn_Xi_tr, tdnn_Ai_tr, tdnn_Ts_tr, tdnn_EWs_tr, tdnn_shift] = ...
+    [tdnn_Xs_tr, tdnn_Xi_tr, tdnn_Ai_tr, tdnn_Ts_tr, ~, ~] = ...
         preparets(tdnn, num2cell(tr_input), num2cell(tr_target));
     % RNN training set
-    [rnn_Xs_tr, rnn_Xi_tr, rnn_Ai_tr, rnn_Ts_tr, rnn_EWs_tr, rnn_shift] = ...
+    [rnn_Xs_tr, rnn_Xi_tr, rnn_Ai_tr, rnn_Ts_tr, ~, ~] = ...
         preparets(rnn, num2cell(tr_input), num2cell(tr_target));
     % TDNN validation set
-    [tdnn_Xs_val, tdnn_Xi_val, tdnn_Ai_val, tdnn_Ts_val, tdnn_EWs_val, tdnn_shift] = ...
+    [tdnn_Xs_val, tdnn_Xi_val, tdnn_Ai_val, tdnn_Ts_val, ~, ~] = ...
         preparets(tdnn, num2cell(tr_input), num2cell(tr_target));
     % RNN validation set
-    [rnn_Xs_val, rnn_Xi_val, rnn_Ai_val, rnn_Ts_val, rnn_EWs_val, rnn_shift] = ...
+    [rnn_Xs_val, rnn_Xi_val, rnn_Ai_val, rnn_Ts_val, ~, ~] = ...
         preparets(rnn, num2cell(tr_input), num2cell(tr_target));
     
     % train the nets
     [trained_tdnn, tdnn_hist] = train(tdnn, tdnn_Xs_tr, tdnn_Ts_tr, tdnn_Xi_tr, tdnn_Ai_tr);
     [trained_rnn, rnn_hist] = train(rnn, rnn_Xs_tr, rnn_Ts_tr, rnn_Xi_tr, rnn_Ai_tr);
     
-    % validation
+    % validation TDNN
     tdnn_val_out = tdnn(tdnn_Xs_val, tdnn_Xi_val, tdnn_Ai_val);
     mse_val = immse(tdnn_val_out, tdnn_Ts_val);
 end
